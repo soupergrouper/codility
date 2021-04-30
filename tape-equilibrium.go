b46f4d3ceaac28bb9d9cc7b35a3f60c852a13eed
soupergrouper <soupergrouper@gmail.com>
2021-04-30 11:08:27 +0300

    Create tape-equilibrium.go

---
+++ tape-equilibrium.go
@@ -0,0 +1,28 @@
+package solution
+
+// you can also use imports, for example:
+// import "fmt"
+// import "os"
+import "math"
+
+// you can write to stdout for debugging purposes, e.g.
+// fmt.Println("this is a debug message")
+
+func Solution(A []int) int {
+    sumR := 0
+    for i := 1; i < len(A); i++ {
+        sumR+=A[i]
+    }
+    sumL := A[0]
+    min := int(math.Abs(float64(sumL) - float64(sumR)))
+    for i := 1; i < len(A)-1; i++ {
+        diff := int(math.Abs(float64(sumL) - float64(sumR)))
+        if diff < min {
+            min = diff
+        }
+        sumL+=A[i]
+        sumR-=A[i]
+    }
+
+    return min
+}
